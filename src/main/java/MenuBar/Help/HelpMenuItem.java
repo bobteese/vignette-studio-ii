3a4fca4576ac32ee7f7661da588b8b9c8c30ada7
AsmitaHari <50839635+AsmitaHari@users.noreply.github.com>
2020-12-14 15:31:16 -0500

    About us Dialog

--- src/main/java/MenuBar/Help/HelpMenuItem.java
+++ src/main/java/MenuBar/Help/HelpMenuItem.java
@@ -2,6 +2,10 @@ package MenuBar.Help;
 
 import DialogHelper.DialogHelper;
 import javafx.scene.control.Alert;
+import javafx.scene.web.WebView;
+
+import javax.swing.*;
+import java.awt.*;
 
 
 public class HelpMenuItem implements HelpMenuItemInterface {
@@ -18,7 +22,7 @@ public class HelpMenuItem implements HelpMenuItemInterface {
          }
          else if (menuName.equals("About")) {
 
-              message = "<html><div style=\"width:350px\">Vignette Studio was created by the Vignette Dreamers as an " +
+              message = "<html><div style=\"font-size:18px\">Vignette Studio was created by the Vignette Dreamers as an " +
                       "undergraduate senior project at Rochester Institute of Technology. Vignette Studio was created for the " +
                       "<a href=\"http://livephoto.rit.edu/\">LivePhoto Physics</a> project. Dr. Robert Teese and Professor Tom Reichlmayr " +
                       "sponsored the project, and Dr. Scott Hawker coached the team. Contributors include:<br><br><p>The Vignette Dreamers:<br>Peter-John Rowe, " +
@@ -27,10 +31,18 @@ public class HelpMenuItem implements HelpMenuItemInterface {
                       "Robert Liedka, Nicolas McCurdy, Hector Pieiro II, Chirag Chandrakant Salian, Angel Shiwakoti, Nils Sohn, Brian Soulliard, " +
                       "Juntian Tao, Gordon Toth, Devin Warren, Alexander Wilczek, Todd Williams, Brian Wyant, Asmita Hari, Jiwoo Baik and Felix Brink.<br><br>Vignette Studio " +
                       "is &copy; 2014-2018, the LivePhoto Physics Project at Rochester Institute of Technology. Vignette Studio is licensed to you under the terms of the GNU General Public License (GPL). " +
-                      "The terms of the license can be found at <a href=\"http://www.gnu.org/licenses/gpl.html\">http://www.gnu.org/licenses/gpl.html</a></div></html>";
-             // DialogHelper helper = new DialogHelper(Alert.AlertType.CONFIRMATION, "About",null, message,
-                                         //  true);
-            new AboutDialog(null).setVisible(true);
+                      "The terms of the license can be found at <a href=\"http://www.gnu.org/licenses/gpl.html\">http://www.gnu.org/licenses/gpl.html</a>" +
+                      "<p style=\"text-align: center;\">Vignette Studio version 1.0</p>" +
+                      "<p style=\"text-align: center;\">Java version"+ JavaVersion.getFullVersion()+"</p>" +
+                      "</div></html>";
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("About Vignette Studio");
+            WebView webView = new WebView();
+            webView.getEngine().loadContent(message);
+            alert.getDialogPane().setContent(webView);;
+            alert.showAndWait();
+
 
 
 
