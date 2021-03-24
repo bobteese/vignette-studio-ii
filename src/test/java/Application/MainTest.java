063ecbd85a518572ab6aa97c41b96f77360995d7
AsmitaHari <50839635+AsmitaHari@users.noreply.github.com>
2021-03-24 14:32:19 -0400

    New Jar File

---
+++ src/test/java/Application/MainTest.java
@@ -0,0 +1,23 @@
+package Application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest extends ApplicationTest {
+
+    @Before
+    void start() {
+        try {
+            ApplicationTest.launch(Main.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
